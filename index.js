2b6a778f64bd7fdcf4e8376e78bb6241d3155630
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-02-10 13:16:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (collaborators.hasOwnProperty(key)) {
 activeCollaborators += collaborators[key] + ', '
 }}
 activeCollaborators = activeCollaborators.slice(0, -2);
-cfonts.say('yotsuba\nbot md', {
+cfonts.say('ficct\nbot md', {
 align: 'center',           
 gradient: ['red', 'blue'] 
 })
